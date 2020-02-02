d21449f38f4de081ba1088308a8d8c1c25c0aad1
Mustafa Ertuğrul Erdoğan <mustafaertugrulerdogan@gmail.com>
2020-02-02 04:38:27 +0300

    Score system, ui changes

--- Assets/OverFixed/Scripts/Game/Controllers/GameController.cs
+++ Assets/OverFixed/Scripts/Game/Controllers/GameController.cs
@@ -1,7 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using OverFixed.Scripts.Game.Behaviours.Ships;
+using OverFixed.Scripts.Game.Models.Data;
 using OverFixed.Scripts.Game.Models.Ships;
 using OverFixed.Scripts.Game.Views.Ships;
 using UnityEngine;
@@ -13,14 +13,19 @@ public class GameController : MonoBehaviour
     public List<PlatformBehaviour> PlatformBehaviours;
     public List<ShipInfo> ShipInfos;
 
+    private TeamData _teamData;
     private ShipBehaviour.Pool _shipPool;
     private IList<PlatformBehaviour> _platformBehaviours;
 
     private float _timer;
 
+    private const float MaxDifficulty = 100f; // for test purpose
+    private float _currentDifficulty;
+
     [Inject]
-    private void Construct(ShipBehaviour.Pool shipPool, IList<PlatformBehaviour> platformBehaviours)
+    private void Initialize(TeamData teamData, ShipBehaviour.Pool shipPool, IList<PlatformBehaviour> platformBehaviours)
     {
+        _teamData = teamData;
         _shipPool = shipPool;
         _platformBehaviours = platformBehaviours;
     }
@@ -35,6 +40,15 @@ public class GameController : MonoBehaviour
     }
 
     private void Update()
+    {
+        //check game condition
+
+        //CheckGameCondition();
+
+        SendShip();
+    }
+
+    private void SendShip()
     {
         if (_timer <= 0 && SendShipToAvailablePlatform())
         {
@@ -57,36 +71,63 @@ public class GameController : MonoBehaviour
 
         if (availablePlatforms.Any())
         {
-            var shipBehaviour = _shipPool.Spawn();
+            ShipInfo info = new ShipInfo();
 
-            var selectedPlatform = availablePlatforms[Random.Range(0, availablePlatforms.Count)].Platform;
-
-            selectedPlatform.IsPlatformOccupied = true;
-            var t = shipBehaviour.transform;
-            t.position = selectedPlatform.SpawnPosition;
-            t.position = selectedPlatform.SpawnPosition;
-            t.rotation = selectedPlatform.SpawnRotation;
-
-
-            //difficulty calculation here
-            shipBehaviour.Ship = new Ship(ShipInfos[Random.Range(0,ShipInfos.Count)], selectedPlatform); 
-
-            var shipView = shipBehaviour.GetComponent<ShipView>();
-            if (shipView != null)
+            if (SelectShipInfo(ref info))
             {
-                shipView.Bind(shipBehaviour.Ship);
+                _currentDifficulty += info.DifficultyPoint;
+
+                var shipBehaviour = _shipPool.Spawn();
+                var selectedPlatform = availablePlatforms[Random.Range(0, availablePlatforms.Count)].Platform;
+
+                selectedPlatform.IsPlatformOccupied = true;
+
+                var t = shipBehaviour.transform;
+                t.position = selectedPlatform.SpawnPosition;
+                t.position = selectedPlatform.SpawnPosition;
+                t.rotation = selectedPlatform.SpawnRotation;
+                
+                shipBehaviour.Ship = new Ship(ShipInfos[Random.Range(0,ShipInfos.Count)], selectedPlatform); 
+
+                var shipView = shipBehaviour.GetComponent<ShipView>();
+                if (shipView != null)
+                {
+                    shipView.Bind(shipBehaviour.Ship);
+                }
+
+                var shipSectionBehaviours = shipBehaviour.GetComponentsInChildren<ShipSectionBehaviour>().ToList();
+                for (var i = 0; i < shipSectionBehaviours.Count; i++)
+                {
+                    shipSectionBehaviours[i].Init(shipBehaviour, shipBehaviour.Ship.ShipSections[i]);
+                }
+
+                Debug.Log($"Current Difficulty {_currentDifficulty}/100");
+
+                shipBehaviour.Init(shipBehaviour.Ship.Info.State, f =>
+                {
+                    _teamData.WarStatus += f;
+                    _currentDifficulty -= info.DifficultyPoint;
+
+
+                    Debug.Log($"WarStatus: {_teamData.WarStatus}");
+                    Debug.Log($"Current Difficulty {_currentDifficulty}/100");
+                }); 
+                shipBehaviour.Land();
+                return true;
             }
+        }
 
-            var shipSectionBehaviours = shipBehaviour.GetComponentsInChildren<ShipSectionBehaviour>().ToList();
-            for (var i = 0; i < shipSectionBehaviours.Count; i++)
-            {
-                shipSectionBehaviours[i].Init(shipBehaviour, shipBehaviour.Ship.ShipSections[i]);
-            }
+        return false;
+    }
 
-            Debug.Log(shipBehaviour.Ship.Info.State);
+    private bool SelectShipInfo(ref ShipInfo info)
+    {
+        var budget = MaxDifficulty - _currentDifficulty;
 
-            shipBehaviour.Init(shipBehaviour.Ship.Info.State); 
-            shipBehaviour.Land();
+        var availableShips = ShipInfos.Where(s => s.DifficultyPoint < budget).ToList();
+        if (availableShips.Any())
+        {
+            info = availableShips[Random.Range(0, availableShips.Count)];
             return true;
         }
 
