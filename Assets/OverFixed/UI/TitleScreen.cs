a121ac3d48f1a744d3be0de8566b47761c426995
BLEDA\Bleda <irdcare@hotmail.com>
2020-02-02 14:12:34 +0300

    Title Screen

---
+++ Assets/OverFixed/UI/TitleScreen.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleScreen : MonoBehaviour
+{
+    public GameObject creditsOverlay;
+
+    public void StartGame()
+    {
+        Application.LoadLevel(1);
+    }
+
+    public void Credits()
+    {
+        creditsOverlay.SetActive(true);
+    }
+
+    public void CreditsClose() {
+        creditsOverlay.SetActive(false);
+    }
+
+    public void ExitGame() {
+        Application.Quit();
+    }
+}
